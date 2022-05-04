9c1915830672761d9116d131bd7846cca7667740
Wanli Xue <xuewanli.lee@hotmail.com>
2022-05-04 14:27:24 +1000

    update

--- grammarchecker.py
+++ grammarchecker.py
@@ -1,17 +1,24 @@
-# Spell and Grammer Checker
+# Spell and Grammar Checker
 # pip install textblob
 # pip install language-tool-python
 from textblob import TextBlob
 import language_tool_python
+
+
 # Spell Checker
 def spell_checker(text):
     text = TextBlob(text)
     print(text.correct())
-# Grammer Checker
-def grammer_checker(text):
+
+
+# Grammar Checker
+def grammar_checker(text):
     tool = language_tool_python.LanguageTool('en-US')
     checker = tool.check(text)
     print("Issue: ", checker[0].ruleIssueType)
     print("Corrected Text:", checker[0].replacements)
-grammer_checker("I Stackoverflow love")
+    print(checker)
+
+
+grammar_checker("I Stackoverflow love")
 spell_checker("The Smell of Fliwers")
